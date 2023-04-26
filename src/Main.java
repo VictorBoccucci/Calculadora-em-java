e0e41a799987cb8ff1ec89efccf387ea5a99e264
Victor Luiz Viana Boccucci <victor.boccucci@engesoftware.com.br>
2023-04-26 19:22:02 -0300

    'Calculadorajava'

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -7,32 +8,45 @@ public class Main {
         Double valorUm;
         Double valorDois;
         String operacao;
+        char continuar = 'S';
 
-        System.out.println("Digite o valor um: ");
-        valorUm = sc.nextDouble();
 
-        System.out.println("Digite a operação( +, -, /, *,): ");
-        operacao = sc.next();
+        while (continuar == 'S' || continuar == 's'){
+            System.out.println("Digite o valor um: ");
+            valorUm = sc.nextDouble();
 
-        System.out.println("Digite o valor dois: ");
-        valorDois = sc.nextDouble();
+            System.out.println("Digite o valor dois: ");
+            valorDois = sc.nextDouble();
 
-        System.out.println("Resultado:" + realizarCalculo(valorUm, valorDois, operacao));
+
+            System.out.println("Digite a operação( 1 Soma, 2 Subtração, 3 Multiplicação, 4 Divisão,): ");
+            operacao = sc.next();
+
+
+            System.out.println("Resultado:" + realizarCalculo(valorUm, valorDois, operacao));
+
+            System.out.println("Deseja realizar uma nova operação? (S ou N): ");
+            continuar = sc.next().charAt(0);
+
+        }
+        sc.close();
     }
+
+
 
     public static Double realizarCalculo(Double valorUm, Double valorDois, String operacao){
         Double respostaCalculo = 0.0;
         switch (operacao){
-            case "+":
+            case "1":
                 respostaCalculo = valorUm + valorDois;
                 break;
-            case "-":
+            case "2":
                 respostaCalculo = valorUm - valorDois;
                 break;
-            case "*":
+            case "3":
                 respostaCalculo = valorUm * valorDois;
                 break;
-            case "/":
+            case "4":
                 respostaCalculo = valorUm / valorDois;
                 break;
 
