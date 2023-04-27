ff4772dbc6d0eb563caae1fadab7e51bd808fa2c
victorboccucci <victorboccucci@gmail.com>
2023-04-27 18:03:35 -0300

    rv

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ public class Main {
 
         Double valorUm;
         Double valorDois;
-        String operacao;
+        int operacao;
         char continuar = 'S';
 
 
-        while (continuar == 'S' || continuar == 's'){
+        do{
             System.out.println("Digite o valor um: ");
             valorUm = sc.nextDouble();
 
@@ -20,40 +20,66 @@ public class Main {
 
 
             System.out.println("Digite a operação( 1 Soma, 2 Subtração, 3 Multiplicação, 4 Divisão,): ");
-            operacao = sc.next();
+            operacao = sc.nextInt();
 
 
             System.out.println("Resultado:" + realizarCalculo(valorUm, valorDois, operacao));
 
             System.out.println("Deseja realizar uma nova operação? (S ou N): ");
-            continuar = sc.next().charAt(0);
+            continuar = sc.next().toUpperCase().charAt(0);
 
-        }
+        }while (continuar == 'S');
         sc.close();
     }
 
 
 
-    public static Double realizarCalculo(Double valorUm, Double valorDois, String operacao){
+    public static Double realizarCalculo(Double valorUm, Double valorDois, int operacao){
         Double respostaCalculo = 0.0;
-        switch (operacao){
-            case "1":
-                respostaCalculo = valorUm + valorDois;
-                break;
-            case "2":
-                respostaCalculo = valorUm - valorDois;
-                break;
-            case "3":
-                respostaCalculo = valorUm * valorDois;
-                break;
-            case "4":
-                respostaCalculo = valorUm / valorDois;
-                break;
 
-            default:
-                System.out.println("Operação é invalida ");
+//        switch (operacao) {
+//            case "1":
+//                respostaCalculo = valorUm + valorDois;
+//                break;
+//            case "2":
+//                respostaCalculo = valorUm - valorDois;
+//                break;
+//            case "3":
+//                respostaCalculo = valorUm * valorDois;
+//                break;
+//            case "4":
+//                respostaCalculo = valorUm / valorDois;
+//                break;
+//
+//            default:
+//                System.out.println("Operação é invalida ");
+//
+//        }
 
+        // Usei if, else if e else para fazer de outra forma e aumentar meu aprendizado
+
+        if (operacao == 1 ) {
+            respostaCalculo = valorUm + valorDois;
+
+
+        } else if (operacao == 2) {
+            respostaCalculo = valorUm - valorDois;
+
+
+
+        }  else if (operacao == 3) {
+            respostaCalculo = valorUm * valorDois;
+
+
+
+        }  else if (operacao == 4) {
+            respostaCalculo = valorUm / valorDois;
+
+
+        } else {
+            System.out.println("Operação é invalida ");
         }
+
 
         return respostaCalculo;
     }
